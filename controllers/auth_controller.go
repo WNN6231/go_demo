3058a5f76232e7ba955ed2f58b525a445ea20b55
WNN6231 <3429236231@qq.com>
2026-04-11 16:12:12 +0800

    模型绑定

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,2 +1,17 @@
 package controllers
 
+import (
+	"exchangeapp/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(ctx *gin.Context) {
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+}
