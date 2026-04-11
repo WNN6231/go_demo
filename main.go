2a2960824bd3be84f67deee9cb6f1e49aba71f39
WNN6231 <3429236231@qq.com>
2026-04-11 14:08:14 +0800

    初次体验Gin

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"exchangeapp/config"
-	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
-	fmt.Println(config.AppConfig.App.Port)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + config.AppConfig.App.Port)
 }
