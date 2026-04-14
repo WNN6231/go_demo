5a1eae439a65a49eddeee8e43d16abdcfdcb4049
WNN6231 <3429236231@qq.com>
2026-04-14 20:31:12 +0800

    实现文章模块功能

--- controllers/exchange_rate_controller.go
+++ controllers/exchange_rate_controller.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"errors"
 	"exchangeapp/global"
 	"exchangeapp/models"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func CreateExchangeRate(ctx *gin.Context) {
@@ -35,10 +37,12 @@ func GetExchangeRates(ctx *gin.Context) {
 	var exchangeRates []models.ExchangeRate
 
 	if err := global.Db.Find(&exchangeRates).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
 		return
 	}
 	ctx.JSON(http.StatusOK, exchangeRates)
 }
-
-
