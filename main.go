5e942fd01db2c47d4b651b8b8bf26efb86e352fd
WNN6231 <3429236231@qq.com>
2026-04-16 17:49:01 +0800

    优雅的退出应用

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"exchangeapp/config"
 	"exchangeapp/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -15,5 +21,26 @@ func main() {
 		port = ":8080"
 	}
 
-	r.Run(port)
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %s\n", err)
+	}
+	log.Println("Server exited properly")
 }
